85ec15e711592aa5797f3d7358af4359db2549bd
Vladyslav Lietun <fyks1649@gmail.com>
2020-04-30 19:10:54 +0300

    init color switch game

---
+++ ColorSwitch/Template/Settings.swift
@@ -0,0 +1,15 @@
+//
+//  Settings.swift
+//  ColorSwitch
+//
+//  Created by Vladyslav Lietun on 30.04.2020.
+//  Copyright © 2020 Vladyslav Lietun. All rights reserved.
+//
+
+import SpriteKit
+
+enum PhysicsCategories {
+    static let none: UInt32 = 0
+    static let ballCategories: UInt32 = 0x1         // 01
+    static let switchCategory: UInt32 = 0x1 << 1    // 10
+}
